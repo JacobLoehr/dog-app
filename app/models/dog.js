f1dc8d4b9e611aff499aa0d2714706b1499b6f56
JacobLoehr <jacob@aibox.ai>
2023-06-14 18:01:12 -0500

    went back to get it working, commiting newer componenets after this

--- app/models/dog.js
+++ app/models/dog.js
@@ -1,15 +1,16 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class DogModel extends Model {
-  @attr name;
-  @attr breed;
-  @attr owner;
-  @attr size;
-  @attr description;
-  @attr breakfast;
-  @attr dinner;
-  get meals(){
-    return `Breakfast: ${this.breakfast} Dinner: ${this.dinner}`;
-  }
+  @attr('string') name;
+  @attr('string') breed;
+  @attr('string') owner;
+  @attr('string') size;
+  @attr('string') description;
+  //@attr breakfast;
+  //@attr dinner;
+  //get meals(){
+    //return `Breakfast: ${this.breakfast} Dinner: ${this.dinner}`;
+  //}
+  @attr meals;
   @attr activites;
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,18 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { getJSON } from '@ember/ajax';
+import { service } from "@ember/service";
+import dogs from './dogs';
 
 export default class IndexRoute extends Route {
   @service store;
 
   async model() {
-    console.log(dogs);
-    //let response = await (await fetch('/api/dogs.json')).json();
-    // Fetch the dog data
-    const response = await getJSON('/dogs.json');
-    const dogsData = response.data;
-    // Load the dog data into the Ember Data store
-    const dogs = dogsData.map(dog => {
-      return this.store.createRecord('dog', dog);
-    });
-    //return this.store.findAll('dogs');
-  }
+    /** 
+    console.log(dogs)
+    let response = await (await fetch('/api/dogs.json')).json()
+    for (const dog of response.data) {
+      this.store.createRecord('dog', dog)
+    }
+    */
+    return response.data;
+}
 }
